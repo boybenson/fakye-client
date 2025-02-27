4df90d5a4fff814a90fd80df02b9e64bfccfd9d3
boybenson <ybenson96@gmail.com>
2025-02-27 12:32:38 +0000

    feat: changes

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -28,7 +28,27 @@ const Home = () => {
         </View>
       </SafeAreaView>
       <View>
-        <View></View>
+        <View className="w-[96%] mx-auto">
+          <View className="border border-gray-400 mt-3 rounded-lg p-2">
+            <View className="flex flex-row items-center justify-between">
+              <View className="flex flex-row items-center">
+                <Image
+                  source={require("../../../assets/images/pp.jpeg")}
+                  style={{ width: 32, height: 32, borderRadius: "100%" }}
+                />
+                <AppText text="Humble" style="text-sm ml-2" />
+              </View>
+              <AppText text="5min ago" style="text-xs text-main_gray" />
+            </View>
+            <View className="mt-2">
+              <AppText
+                text="The description of the item you want to dash for free goes here and can be long to occupy three lines"
+                style="text-xs text-main_gray"
+              />
+            </View>
+            <View></View>
+          </View>
+        </View>
       </View>
     </View>
   );
